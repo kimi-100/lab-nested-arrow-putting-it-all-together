54d4adda7b13c33ce9f65b82dd0c8898bbf7cdb7
Victor Kiptoo Kimitei <kimiteivictor@gmail.com>
2026-04-21 18:24:54 +0300

    Solution 2

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // CREATED OBJECT CONTAINING USER INFO/
 const userInfo = {
-  username: "User",
-  password: "Abc123",
+  username: "user1",
+  password: "password123",
 };
 
 const createLoginTracker = function (userInfo) {
   let attemptCounter = 0;
   //INNER FUNCTION WITH PASSWORD ATTEMPT LOGIC
   const passwordAttemptLogic = (passwordAttempt) => {
-    if (attemptCounter >=3) {
+    if (attemptCounter >3) {
       let returnMessage = `Account locked due to too many failed login attempts`;
       return returnMessage;
 
     } else if (passwordAttempt === userInfo.password) {
-      let returnMessage = `Welcome ${userInfo.username}`;
+      let returnMessage = `Login successful`;
       return returnMessage;
 
     } else {
-      let returnMessage = `Incorrect Password`;
       attemptCounter++;
+      let returnMessage = `Attempt ${attemptCounter}: Login failed`;
       return returnMessage;
     }
   }
@@ -30,7 +30,7 @@ const login = createLoginTracker(userInfo);
 console.log(login("wrongAttemptOne"));
 console.log(login("wrongAttemptTwo"));
 // console.log(login("wrongAttemptThree"));
-console.log(login("Abc123"));
+console.log(login("password123"));
 
 module.exports = {
   ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
